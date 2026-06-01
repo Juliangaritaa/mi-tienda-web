fbfa3540761150f60a521b8118217cdd62bcd644
Julian Felipe Angarita Alarcon <julian.angarita11@soy.sena.edu.co>
2026-06-01 16:27:37 -0500

    feat: add login types intereface

---
+++ src/shared/types/auth.type.ts
@@ -0,0 +1,12 @@
+export interface LoginRequest {
+    username: string;
+    password: string;
+}
+
+export interface LoginResponse {
+    user: {
+        id: string;
+        username: string;
+    };
+    token: string;
+}
