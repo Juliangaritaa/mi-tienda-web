a19572d687e2b35418fe1176b073a3cc838a0276
Julian Felipe Angarita Alarcon <julian.angarita11@soy.sena.edu.co>
2026-06-01 20:30:32 -0500

    feat: add dashboard layout component

--- src/app/router/route-paths.tsx
+++ src/app/router/route-paths.tsx
@@ -1,6 +1,8 @@
+//Define las rutas exportables de los módulos de la app
+
 export const ROUTES = {
     LOGIN: "/login",
-    DASHBOARD: "/",
+    DASHBOARD: "/", //Todo usuario autenticado ingresa al dashboard, de lo contrario será devuelto a login
     PRODUCTS: "products",
     CATEGORIES: "/categories",
     TICKETS: "/tickets",
