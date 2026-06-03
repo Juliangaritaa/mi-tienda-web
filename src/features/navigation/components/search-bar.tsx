38b9a0572fbd83a8f52a2041921e3310bf688199
Julian Felipe Angarita Alarcon <julian.angarita11@soy.sena.edu.co>
2026-06-03 13:42:24 -0500

    feat: add serach bar component function

---
+++ src/features/navigation/components/search-bar.tsx
@@ -0,0 +1,11 @@
+import { Search } from "lucide-react";
+import { Input } from "@/components/input";
+
+export function SearchBar() {
+    return (
+        <div className="relative w-96">
+            <Search className="absolute left-3 top-3 h-4 w-4" />
+            <Input placeholder="Buscar..." className="pl-9" />
+        </div>
+    );
+}
